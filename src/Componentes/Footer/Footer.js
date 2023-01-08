9b7ee9cc9fcc0a5c90dfebc090f13f9f1b502887
SLautaro <santillanlautaronahuel@gmail.com>
2023-01-07 22:28:31 -0300

    responsive

--- src/Componentes/Footer/Footer.js
+++ src/Componentes/Footer/Footer.js
@@ -2,33 +2,41 @@ import React from "react";
 import "boxicons";
 const Footer = () => {
   return (
-    <div className="footer">
+    <footer>
       <div className="containfooter">
-        <h4>TraderPRo</h4>
-        <ul className="List">
-          <li>
-            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-              <box-icon type="logo" name="instagram"></box-icon>
-            </a>
-            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-              <box-icon type="logo" name="facebook-square"></box-icon>
-            </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
-              <box-icon type="logo" name="twitter"></box-icon>
-            </a>
-            <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
-              <box-icon type="logo" name="youtube"></box-icon>
-            </a>
-            <a href="https://www.twitch.com" target="_blank" rel="noreferrer">
-              <box-icon type="logo" name="twitch"></box-icon>
-            </a>
-          </li>
-        </ul>
-        <small>
-          &copy; Copyright 2022.TraderPRo | Todos los derechos reservados.
-        </small>
+        <p>TraderPRo</p>
+        <div className="footer-a">
+          <a href="tel:543814982513">Numero</a>
+          <a href="mailto:alexxxfernandez50@gmail.com">
+            Correo
+          </a></div>
+        <div>
+          <ul className="List">
+            <li>
+              <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                <box-icon type="logo" name="instagram"></box-icon>
+              </a>
+              <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+                <box-icon type="logo" name="facebook-square"></box-icon>
+              </a>
+              <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+                <box-icon type="logo" name="twitter"></box-icon>
+              </a>
+              <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
+                <box-icon type="logo" name="youtube"></box-icon>
+              </a>
+              <a href="https://www.twitch.com" target="_blank" rel="noreferrer">
+                <box-icon type="logo" name="twitch"></box-icon>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+
+      <p>&copy; Copyright 2022.TraderPRo | Todos los derechos reservados.</p>
+
+    </footer>
+
   );
 };
 export default Footer;
