ee31cea978be06a12ba79ef163e3b1d1d2d18383
SLautaro <111198390+LautaroNSantillan@users.noreply.github.com>
2023-01-23 21:33:41 -0300

    Style overhaul

---
+++ src/Componentes/cursosBoxes/cursos.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+ const CursosBoxes = () => {
+   return (
+    <section>
+   
+   <div className="container text-center">
+  <div className="row">
+    <div className="col-4">
+      <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+    <div className="col-4">
+    <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+    <div className="col-4">
+    <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+  </div>
+  <div className="row">
+    <div className="col-3">
+    <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+    <div className="col-6">
+    <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+    <div className="col-3">
+    <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+  </div>
+  <div className="row">
+    <div className="col-4">
+      <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+    <div className="col-4">
+    <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+    <div className="col-4">
+    <a href="" className="btn-a"> <span className="btn-span">CURSOS</span> <i className="btn-i"></i> </a>
+    </div>
+  </div>
+</div>
+
+      </section>)
+  }
+  export default CursosBoxes;
