9db197d29cbe5f20f84628ce8d91387cd4c5c723
SLautaro <111198390+LautaroNSantillan@users.noreply.github.com>
2023-01-28 14:33:14 -0300

    modal derecha

--- src/Componentes/inicio/Inicio.jsx
+++ src/Componentes/inicio/Inicio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeBanner from "../home/home-comp";
 import CursosBoxes from "../cursosBoxes/cursos";
+import ModalLogin from "../modal/modal-login";
 const Inicio = () => {
   return (
     <main>
