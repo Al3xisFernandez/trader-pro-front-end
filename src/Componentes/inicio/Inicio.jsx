922b4426e7ddba4d315834a4761680d3a6742b48
Al3xisfernandez <alexxxfernandez50@gmail.com>
2023-02-04 17:37:52 -0300

    prueba

--- src/Componentes/inicio/Inicio.jsx
+++ src/Componentes/inicio/Inicio.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import HomeBanner from "../home/home-comp";
 
-import CursosBoxes from "../cursosBoxes/cursos"
+import CursosBoxes from "../cursosBoxes/cursos";
 
 const Inicio = () => {
   const isLoggedIn = window.localStorage.getItem("loggedIn");
 
   return (
     <main>
-      
       {isLoggedIn ? (
-            <>
-            <HomeBanner />
-              <CursosBoxes />
-            </>
-          ) : (
-            <HomeBanner />
-          )}
+        <>
+          <HomeBanner />
+          <CursosBoxes />
+        </>
+      ) : (
+        <HomeBanner />
+      )}
     </main>
   );
 };
